8ffbbb27d3a734005e02f6184a3881eb412425c4
Haripriyaaaaa <100415895+Haripriyaaaaa@users.noreply.github.com>
2022-03-03 15:34:20 +0530

    Add files via upload

---
+++ AccessModifier.java
@@ -0,0 +1,16 @@
+
+
+public class AccessModifier {
+	
+
+		public static void main(String[] args) {
+			System.out.println("Dafault Access Specifier");
+			DefAccessSpecifier obj = new DefAccessSpecifier(); 		  
+	       obj.display(); 
+
+
+		}
+
+	}
+
+
